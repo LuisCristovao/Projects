94a969a28936b26d3f61ae1fdadaf2ee57fc25b3
secretid <secretidentity009@gmail.com>
2018-02-11 22:56:45 +0000

    first images animation done

--- AnimationProject/changeFileNames.py
+++ AnimationProject/changeFileNames.py
@@ -6,8 +6,16 @@ Created on Sun Feb 11 21:14:02 2018
 """
 
 import os
-directory="images/Idle/"
+import cv2
+
+
+
+
+
+directory="images/Intro/"
 files=os.listdir(directory)
 for i in range(len(files)):
-    os.rename(directory+files[i],directory+str(i)+".jpg")
+    #os.rename(directory+files[i],directory+str(i)+".png")
+    im = cv2.imread(directory+files[i])
+    cv2.imwrite(directory+str(i)+".jpg",im)
     print(files[i])
