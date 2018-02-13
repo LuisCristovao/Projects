6db7adb35b437cc3a6a446d37ed3d76f0c677d14
secretid <secretidentity009@gmail.com>
2018-02-13 10:45:46 +0000

    almost complet attack power up

--- AnimationProject/changeFileNames.py
+++ AnimationProject/changeFileNames.py
@@ -12,7 +12,7 @@ import cv2
 
 
 
-directory="images/GoodEvolve/Health/0/"
+directory="images/GoodEvolve/Defense/0/"
 files=os.listdir(directory)
 for i in range(len(files)):
     os.rename(directory+files[i],directory+str(i)+".jpg")
