f5a60527590c3762e7ece074c62c2c982b4b9ac3
secretID2 <secretidentity009@gmail.com>
2018-03-12 13:24:32 +0000

    Starting check winner
    Need to think a general solution make this. i could be possible doing a function that reads all matrix One and saves where it appears '1' and '2'.  See if any '1's or 2's have two time neighbours or matrix_size-1 neighbours

--- TicTacToe/Tic.js
+++ TicTacToe/Tic.js
@@ -17,6 +17,8 @@ class Brain{
         
         return number_of_differences;
     }
+    //If we already now the state, we need to find the best play for that state
+    
     static SelectBestPlay(plays){
         //0-reward;1-play
         //initialize with 0 because all rewards are higher than 0
@@ -30,21 +32,29 @@ class Brain{
         }
         return max_play[1];
     }
+    //function that puts '1' in the matrix where there is a zero
+    //state has form '100000100'
     static GenerateRandomPlay(state){
         var possibilities=['0','1'];
-        var result='';
+        var result=state;
+        var zeros=[];
+        //see zeros
         for(i=0;i<10;i++){
             //n equals 0 or 1;
-            var n=Math.random();
-            if(){
-               
+            if(state[i]=='0'){
+                zeros.push(i);
             }
-            result+=possibilities[n];
         }
+        //select random pos where there is a zero
+        var n =Math.floor(Math.random()*zeros.length);
+        var rindex=zero[n];
+        //play
+        result[rindex]='1';
         return result;
     }
-    
-    SelectBest(actual_state){
+    //Selects most similar state from saved states
+    //dont know if just select when its equal or similar
+    /*SelectBest(actual_state){
         //min saves number of differences [0] and state [1]
         //initialize with 10 because the array could never have more than 10 differences
         var min=[10,'000000000'];
@@ -59,6 +69,10 @@ class Brain{
         //return state from brain with less differences
         //meaning more similar
         return min[1];
+    }*/
+    //actual state is a string like '002100000'
+    SelectEqualState(actual_state){
+        return this.states[actual_state]
     }
     //add play to database
     AddPlay(state,play){
@@ -85,7 +99,8 @@ class Brain{
             if(Object.keys(this.states).length>0){
                 
                 //select closer state in brain to the received state
-                var best_state= this.SelectBest(state);
+                //var best_state= this.SelectBest(state);
+                var best_state= this.SelectEqualState(state);
                 var plays= this.states[best_state];
                 //select best play
                 return this.SelectBestPlay(plays);
@@ -113,6 +128,7 @@ class DrawCanvas{
         this.ctx=this.c.getContext('2d');
         this.matrix_size=matrix_size;
         this.matrix=[];
+        this.Initialize();
     }
     Initialize(){
         var width=this.c.width;
@@ -130,10 +146,25 @@ class DrawCanvas{
         
     }
     //access matrix position like pixels
-    Draw(x,y,color){
-        var square=this.matrix[x][y];
-        this.fillStyle=color;
-        this.ctx.fillRect(square.x,square.y,square.xsize,square.ysize)
+    Draw(state_matrix){
+        var color;
+        for(i=0;i<state_matrix.length;i++){
+            for(j=0;j<state_matrix[0].length;j++){
+                if(state_matrix[i][j]=='0'){
+                    color='white';
+                }
+                if(state_matrix[i][j]=='1'){
+                    color='blue';
+                }
+                if(state_matrix[i][j]=='2'){
+                    color='red';
+                }
+                var square=this.matrix[x][y];
+                this.fillStyle=color;
+                this.ctx.fillRect(square.x,square.y,square.xsize,square.ysize)
+            }
+        }
+        
     }
     
     
@@ -146,12 +177,14 @@ class Game{
     constructor(brain,matrix_size){
         this.state=0;
         this.OneMatrix=[];
+        
         this.OnePlays=[];
         this.TwoPlays=[];
         this.TwoMatrix=[];
         this.brain=brain;
         this.matrix_size=matrix_size;
         this.draw_canvas=new DrawCanvas(matrix_size);
+        this.Initialize();
     }
     Initialize(){
         for(i=0;i<this.matrix_size;i++){
@@ -166,6 +199,7 @@ class Game{
                 this.TwoMatrix[i][j]='0';
             }    
         }
+        
     }
     
     static MatrixToString(matrix){
@@ -198,6 +232,27 @@ class Game{
             }
         }
     }
+    //replace 'num1' with 'num2'
+    ChangeNumbersInMatrix(num1,num2,matrix){
+         for(i=0;i<this.matrix_size;i++){
+                for(j=0;j<this.matrix_size;j++){
+                    if(matrix[i][j]==num1){
+                        matrix[i][j]=num2;
+                    }
+                }
+            }
+    }
+    CheckWinnner(){
+        //winner[0]=true if player one is winner
+        //winner[1]=true if player two is winner
+        //if both are false than is still game on!
+        var winner=[];
+        for(i=0;i<this.matrix_size;i++){
+                for(j=0;j<this.matrix_size;j++){
+                    
+                }
+        }
+    }
     
 
     GetHumanPlayerPlay(){
@@ -208,24 +263,56 @@ class Game{
         var play=this.brain.Play(state,0.5);
         
     }
-    Play(){
+    //if human is a player or not is the Bot boolean
+    Play(Bot){
         //Player One time to play
         if(this.state==0){
-            
+            //if bot is a player
+            if(Bot){
+                //flatten matrix
+                var actual_game_state=this.MatrixToString(this.OneMatrix);
+                //0.5 is the percentage where the bot plays randomly
+                var botplay= this.brain.Play(actual_game_state,0.5);
+                //change player One matrix
+                this.StringToMatrix(botplay,true);
+                //change player two matrix 
+                this.StringToMatrix(botplay,false);
+                this.ChangeNumbersInMatrix('1','2',this.TwoMatrix);
+                //draw what heppend
+                this.draw_canvas.Draw(this.OneMatrix);
+                this.state=2;
+            }
+            //if human player
+            else{
+                
+            }
         }
-        //alter playerOne Matrix and stuff
-        if(this.state==1){
+        //alter playerOne Matrix and draw
+        //if(this.state==1){
             
-        }
+        //}
         //Player Two time to play
+        //player two is always the bot
         if(this.state==2){
-            
+           //flatten matrix
+                var actual_game_state=this.MatrixToString(this.TwoMatrix);
+                //0.5 is the percentage where the bot plays randomly
+                var botplay= this.brain.Play(actual_game_state,0.5);
+                //change player Two matrix
+                this.StringToMatrix(botplay,false);
+                //change player One matrix 
+                this.StringToMatrix(botplay,false);
+                this.ChangeNumbersInMatrix('1','2',this.OneMatrix);
+                //draw what heppend
+                //One matrix is the reference matrix to draw
+                this.draw_canvas.Draw(this.OneMatrix);
+                this.state=4;
         }
         //alter player two Matrix and stuff
         if(this.state==3){
             
         }
-        //Check winner and Run GameOver
+        //Check winner and Run GameOver meaning save in brain plays
         if(this.state==4){
             
         }
