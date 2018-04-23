fd61c0c5df4dacbed4a89fdb1a27407c9518200b
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-23 21:17:09 +0100

    something nice but not what i am looking for

--- Search_Engine/search.js
+++ Search_Engine/search.js
@@ -5,11 +5,34 @@ function Show(text){
     console.log(text.value);
 }
 function WordArray(text){
-    words=text.value.split(" ");
+    lines=text.value.split("\n");
+    lines.forEach(function(line){
+        line.split(' ').forEach(function(word){
+            words.push(word);
+        });
+    });
+        
+    
 }
 function liveSearch(){
-    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla1</p>');
-    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla333</p>');
+    //$('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla1</p>');
+    //$('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla333</p>');
+    var prev_word="";
+    if(search.value==""){
+        $('#live_search').html("");
+    }
+    else{
+        
+        for(i=0;i<words.length ;i++){
+            if(search.value.localeCompare(words[i])==0){
+                if(words[i].localeCompare(prev_word)!=0){
+                    prev_word=words[i];
+                    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">'+words[i]+'</p>');
+                }
+            }
+        }
+    }
+    
 }
 function on(element,color_str){
     element.style.backgroundColor=color_str;
