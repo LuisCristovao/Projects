d717be6b799dd3b4515b65e8dbcbc0c06654ec21
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-23 19:47:36 +0100

    live search html

--- Search_Engine/search.js
+++ Search_Engine/search.js
@@ -7,5 +7,15 @@ function Show(text){
 function WordArray(text){
     words=text.value.split(" ");
 }
-all_text.addEventListener("input",function(){WordArray(all_text)});
-search.addEventListener("input",function(){Show(search)});
+function liveSearch(){
+    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla1</p>');
+    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla333</p>');
+}
+function on(element,color_str){
+    element.style.backgroundColor=color_str;
+}
+function put(element){
+    search.value=element.innerHTML;
+}
+all_text.addEventListener("input",function(){WordArray(all_text);});
+search.addEventListener("input",function(){liveSearch();});
