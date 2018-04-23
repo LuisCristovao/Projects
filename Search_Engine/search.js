6a2556c88a209af7325b5a3f554da5423c2b54b7
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-23 21:59:01 +0100

    safe state

--- Search_Engine/search.js
+++ Search_Engine/search.js
@@ -1,6 +1,29 @@
 var all_text=document.getElementById("text");
 var search=document.getElementById("search");
 var words=[];
+
+/*class ProLetter{
+    contructor(letter,percentage){
+        this.letter=letter;
+        this.percentage=percentage;
+    }
+}
+
+class ProWords{
+    
+    constructor(){
+    
+    }
+    Study(word){
+        for(i=0;i<word.length;i++){
+            
+        }
+    }
+}*/
+
+
+
+
 function Show(text){
     console.log(text.value);
 }
@@ -17,18 +40,32 @@ function WordArray(text){
 function liveSearch(){
     //$('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla1</p>');
     //$('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">Bla333</p>');
-    var prev_word="";
+    var prev_words=[];
+    prev_words.push("");
+    search_text=search.value.toLowerCase();
     if(search.value==""){
         $('#live_search').html("");
+        prev_words=[];
+        prev_words.push("");
     }
     else{
         
         for(i=0;i<words.length ;i++){
-            if(search.value.localeCompare(words[i])==0){
-                if(words[i].localeCompare(prev_word)!=0){
-                    prev_word=words[i];
-                    $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">'+words[i]+'</p>');
-                }
+            
+            if(search_text.localeCompare(words[i].toLowerCase())==0){
+                console.log(words[i]);
+                prev_words.forEach(function(prev_word){
+                    var count=0;
+                    if(words[i].localeCompare(prev_word)!=0){
+                        count++;
+                    }
+                    if(count==prev_words.length){
+                        
+                        prev_words.push(words[i]);
+                        $('#live_search').append('<p onmouseover="on(this,\'rgb(170,170,170)\')"  onmouseout="on(this,\'rgb(255,255,255)\')" onclick="put(this)">'+words[i]+'</p>');
+                    }
+                });
+                
             }
         }
     }
