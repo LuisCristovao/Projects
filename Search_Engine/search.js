919f13d76572a69f7f436995ab5e90f977fc96eb
LuisCristovao <luis.cristovao92@gmail.com>
2018-04-23 17:23:09 +0100

    Search Engine begining

---
+++ Search_Engine/search.js
@@ -0,0 +1,11 @@
+var all_text=document.getElementById("text");
+var search=document.getElementById("search");
+var words=[];
+function Show(text){
+    console.log(text.value);
+}
+function WordArray(text){
+    words=text.split(" ");
+}
+all_text.addEventListener("input",function(){WordArray(all_text)});
+search.addEventListener("input",function(){Show(search)});
