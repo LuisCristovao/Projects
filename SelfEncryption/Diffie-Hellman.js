5a1f580777cd60f3b62c18fb92b39688e97d32e6
LuisCristovao <luis.cristovao92@gmail.com>
2018-09-05 13:33:14 +0100

    mod for large numbers working
    update generate random number to generate big numbers

--- SelfEncryption/Diffie-Hellman.js
+++ SelfEncryption/Diffie-Hellman.js
@@ -33,9 +33,56 @@ print("\n[Private]Alice: Our Common Key is:",DH(dhBob,Alice_key,mod))
 print("\n[Private]Bob: Our Common Key is:",DH(dhAlice,Bob_key,mod))*/
 
 
-
+//works for numbers lower than 2**50
+//need to do 3 times
 function DH(generator,private_number,mod){
     return (generator**private_number)%mod;
 }
+/*
+Makes mod for big numbers 
+aNumStr is a string with the number not in scientific notation
+aDiv is a number (did not test if it works for really large numbers)
+*/
+function modulo( aNumStr, aDiv)
+{
+	var tmp = "";
+	var  i, r;
+	for ( i=0; i<aNumStr.length ; i++)
+	{
+		tmp += aNumStr.charAt( i);
+		r = tmp % aDiv;
+		tmp = r.toString( 10);
+	}
+	return tmp / 1;
+}
 
-console.log(DH(23000,34,345));
+
+
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var alicekey=getRandomArbitrary(2,12)
+var bobkey=getRandomArbitrary(2,12)
+
+gen=getRandomArbitrary(2,9);
+do{
+    
+    mod=getRandomArbitrary(2,9)*getRandomArbitrary(10,50)
+}while(mod>2**50)
+
+console.log("[Public]Alice: the generator is "+gen+" and the mod is "+mod+"\n");
+console.log("[Private]Alice: my private number is, "+alicekey);
+console.log("[Private]Bob: my private number is, "+bobkey);
+dhAlice=DH(gen,alicekey,mod)
+dhBob=DH(gen,bobkey,mod)
+console.log("\n[Public]Alice: Take my DH number: "+DH(gen,alicekey,mod))
+console.log("[Public]Bob: Take my DH number: "+DH(gen,bobkey,mod))
+
+console.log("\n[Private]Alice: Our Common Key is: "+DH(dhBob,alicekey,mod))
+console.log("\n[Private]Bob: Our Common Key is: "+DH(dhAlice,bobkey,mod))
